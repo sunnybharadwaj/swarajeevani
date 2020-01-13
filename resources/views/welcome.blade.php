c87031fa4d337d6ab5b3daf7a4f5b8cad4dbc9ab
Sunny Bharadwaj <sunny@chakradesign.co>
2020-01-13 16:10:38 +0530

    Develop testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Swarajeevani
+                    Swarajeevani Development
                 </div>
 
                 <div class="links">
